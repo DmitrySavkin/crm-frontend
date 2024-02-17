ca18e3ac79f6709f569610dcde2b88ee407e16fc
Dmitry Savkin <dimsaz@mail.ru>
2024-02-17 12:50:21 +0100

    customer service. Post request

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Customer } from './customer';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { CustomersService } from './service/customers.service';
 
 @Component({
   selector: 'app-customers',
@@ -8,10 +9,11 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.less'
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit {
   IFrameUrl? : SafeResourceUrl;
   displayIFrame = false;
 
+  constructor(private customerService: CustomersService) {}
   customer: Customer = {
 
       id:0,
@@ -30,18 +32,23 @@ export class CustomersComponent {
      }
   ];
 
-  constructor(private sanitizer: DomSanitizer) {
 
+  ngOnInit(): void {
   }
+
   openOptions() {
     console.log("click");
   }
 
-  onSubmit() {
-    console.log(this.customer);
-  }
-  newCustomerDialog() : void {
-      this.IFrameUrl = this.sanitizer.bypassSecurityTrustResourceUrl('/newform');
-      this.displayIFrame = true;
+  
+  submitForm(form: any): void {
+    // Handle form submission here
+    console.log(form.value);
+    this.customerService.addCustomer(form.value).subscribe(response => {
+      console.log('Form data saved successfully:', response);
+      
+    }, error => {
+      console.error('Error saving form data:', error);
+    });
   }
 }
